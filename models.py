372c283ca8aecd1a72146ffaacd904fbab385c3f
mtomberlin <mtomberlin@csumb.edu>
2017-03-04 03:07:57 +0000

    Fixed missing SQLAlchemy ref

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ import os
 import flask_sqlalchemy, app
 
 app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-db = flask_sqlalchemy.SQLAlchemy(app)
+db = flask_sqlalchemy.SQLAlchemy(app.app)
 
 class Message (db.Model):
     id = db.Column(db.Integer,primary_key=True) # key
