7e4d126906004d2ba9f6d669446fe25789250853
mtomberlin <mtomberlin@csumb.edu>
2017-03-06 03:09:15 +0000

    Test

--- tests/socketio.py
+++ tests/socketio.py
@@ -4,7 +4,7 @@ import json
 
 class SocketIOTest(unittest.TestCase):
     def test_no_token_message(self):
-        client = app.socketio.test_client()
+        client = app.socketio.test_client(app.app)
         client.emit('new message', {'facebook_user_token':'','google_user_token':'','message':{'text': "Some text"}})
         r = client.get_received()
         # print r
@@ -17,7 +17,7 @@ class SocketIOTest(unittest.TestCase):
         data = from_server['args'][0]
         self.assertEquals( data['messages'], None )
     def test_no_token_connected(self):
-        client = app.socketio.test_client()
+        client = app.socketio.test_client(app.app)
         client.emit('user connected',  {'facebook_user_token':'','google_user_token':'','username':'TestUser'})
         r = client.get_received()
         # print r
@@ -27,7 +27,7 @@ class SocketIOTest(unittest.TestCase):
         data = from_server['args'][0]
         self.assertEquals( data['messages'], None )
     def test_disconnected(self):
-        client = app.socketio.test_client()
+        client = app.socketio.test_client(app.app)
         client.emit('user disconnected',  {'service':'Google','username':'TestUser'})
         r = client.get_received()
         # print r
@@ -38,7 +38,7 @@ class SocketIOTest(unittest.TestCase):
         self.assertEquals( data['service'], 'Google' )
         self.assertEquals( data['username'], 'TestUser' )
     def test_user_quit(self):
-        client = app.socketio.test_client()
+        client = app.socketio.test_client(app.app)
         client.emit('user quit', {'username':'TestUser'})
         r = client.get_received()
         # print r
